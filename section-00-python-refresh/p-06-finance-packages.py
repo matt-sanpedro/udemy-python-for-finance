848f66d7015efef9838307c172eccdc6c3388cc0
matt-sanpedro <mattsanpedro7@gmail.com>
2025-12-27 14:13:12 -0800

    Corrected spelling error on visualization

--- section-00-python-refresh/p-06-finance-packages.py
+++ section-00-python-refresh/p-06-finance-packages.py
@@ -4,7 +4,7 @@ Docstring for p-06-finance-packages
 NumPy: a third-party package allowing us to work with multidimensional arrays
 Pandas (panel data): allows us to organize data in a tabular form and to attach descriptive 
     labels to the rows and columns of the table
-Matplotlib: a 2d plotting library specifically designed for visuaslization of NumPy arrays
+Matplotlib: a 2d plotting library specifically designed for visualization of NumPy arrays
     a plotting library for creating static, animated, and interactive visualizations in Python
 math: built-in package for mathmematical functions and constants
 random: invokes random number generators
