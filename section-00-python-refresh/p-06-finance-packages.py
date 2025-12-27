ca6d0c3e9d8d8c841586f8762f25755a84347dba
matt-sanpedro <mattsanpedro7@gmail.com>
2025-12-27 12:46:42 -0800

    Added several different examples on how to import finance and data science related Python modules

---
+++ section-00-python-refresh/p-06-finance-packages.py
@@ -0,0 +1,19 @@
+"""
+Docstring for p-06-finance-packages
+
+NumPy: a third-party package allowing us to work with multidimensional arrays
+Pandas (panel data): allows us to organize data in a tabular form and to attach descriptive 
+    labels to the rows and columns of the table
+Matplotlib: a 2d plotting library specifically designed for visuaslization of NumPy arrays
+    a plotting library for creating static, animated, and interactive visualizations in Python
+math: built-in package for mathmematical functions and constants
+random: invokes random number generators
+statsmodels: descriptive statistics, plotting functions, regressions
+"""
+import numpy as np
+import pandas as pd
+import matplotlib
+import math
+import random
+import statsmodels
+import pandas_datareader
